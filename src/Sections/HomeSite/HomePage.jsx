3c4e9d468227ea745da6562bdd2f02e07260416c
ajo <ajoalex012@gmail.com>
2021-07-16 21:28:06 +0530

    nava and hero completed

---
+++ src/Sections/HomeSite/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../../Components/NavBar/NavBar'
+import Hero from '../../Components/Hero/Hero'
+function HomePage() {
+    return (
+        <div>
+           <NavBar /> 
+           <Hero />
+
+        </div>
+    )
+}
+
+export default HomePage
